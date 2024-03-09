7fcd64be7f3d1b04820ee5cdaf851d3c22cb9d22
plan6062 <qwa7854@naver.com>
2024-03-09 15:40:35 +0900

    feat : 포탄, 목표물 추가 및 기능구현
    포탄과 목표물을 추가하고 포탄과 목표물과의 거리를 계산해서 데미지 주는 로직 구현

---
+++ Assets/ShooterRotator.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterRotator : MonoBehaviour
+{
+   private enum RotateState{
+    Idle, Vertical, Horizontal, Ready
+    //처음 상태, 위아래로 회전중, 가로방향으로 회전중, 회전이 끝나고 포탄 
+    //쏠 준비 완료 상태
+   }
+
+   private RotateState state = RotateState.Idle;
+   public float verticalRotateSpeed = 360f;
+   public float horizontalRotateSpeed = 360f;
+
+   void Update()
+   {
+        switch(state){
+            case RotateState.Idle:
+                if(Input.GetButtonDown("Fire1")){
+                    state = RotateState.Horizontal;
+                }
+            break;
+
+            case RotateState.Horizontal:
+                if(Input.GetButton("Fire1")) {
+                    transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
+                } else if(Input.GetButtonUp("Fire1")){
+                    state = RotateState.Vertical;
+                }
+            break;
+
+            case RotateState.Vertical:
+                if(Input.GetButton("Fire1")){
+                    transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0, 0));
+                } else if (Input.GetButtonUp("Fire1")){
+                    state = RotateState.Ready;
+                }
+            break;
+
+            case RotateState.Ready:
+                
+            break;
+        }
+        // if(state == RotateState.Idle){
+        //     if(Input.GetButtonDown("Fire1")){
+        //         state = RotateState.Horizontal;
+        //     }
+        // } 
+        // else if(state == RotateState.Horizontal) {
+        //     if(Input.GetButton("Fire1")) {
+        //         transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
+        //     } else if(Input.GetButtonUp("Fire1")){
+        //         state = RotateState.Vertical;
+        //     }
+        // } 
+        // else if(state == RotateState.Vertical){
+        //     if(Input.GetButton("Fire1")){
+        //         transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0, 0));
+        //     } else if (Input.GetButtonUp("Fire1")){
+        //         state = RotateState.Ready;
+        //     }
+        // } 
+        // if문과 switch문
+    } 
+}
+
